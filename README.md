4147eba76b78539581017a5f1cc45e191c30bd32
Kieran White <kieran@bluesquareoffices.com>
2021-03-07 19:51:23 +0000

    Updating readme with checklist.

--- README.md
+++ README.md
@@ -36,22 +36,22 @@ Finally, the use of third party libraries and/or components is permitted - and i
 
 These are the minimum requirements for the exercies:
 
-1. Retrieve the data from the mock API.
-1. Output the data in a list, including properties from the data that are appropriate for a list view.
-1. Implement a category filter - this can be single or multi-select.
-1. Implement pagination - this can be traditional numbered pages or "load more".
-1. Use semantic markup where possible.
-1. Create a responsive layout with HTML and CSS.
+- [x] Retrieve the data from the mock API.
+- [x] Output the data in a list, including properties from the data that are appropriate for a list view.
+- [x] Implement a category filter - this can be single or multi-select.
+- [x] Implement pagination - this can be traditional numbered pages or "load more".
+- [x] Use semantic markup where possible.
+- [x] Create a responsive layout with HTML and CSS.
 
 ### Additional Exercises
 
 If you have time then demonstrating any of the following would be considered as a bonus:
 
-1. Use client-side routing to create a "detail" page.
-1. Persist filter state in the query string.
-1. Include animated transitions between application state, e.g. when filtering.
-1. Convert the application to use TypeScript instead of JavaScript.
-1. Use a CSS preprocessor or CSS-in-JS rather than plain CSS.
+- [ ] Use client-side routing to create a "detail" page.
+- [ ] Persist filter state in the query string.
+- [x] Include animated transitions between application state, e.g. when filtering.
+- [x] Convert the application to use TypeScript instead of JavaScript.
+- [x] Use a CSS preprocessor or CSS-in-JS rather than plain CSS.
 
 ## Submission
 
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import ScrollToTop from 'react-scroll-to-top';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 import PostsPagination from 'components/pagination/Pagination';
 import Options from 'components/options/Options';
 import Posts from 'components/posts/Posts';
 import Spinner from 'components/spinner/Spinner';
-import { getCategories, getPosts } from 'services';
+import { getPosts, getPostsByCategory } from 'services';
 import { Post } from 'types';
 import { pageSize } from 'config';
 
@@ -19,7 +16,6 @@ const App = (): JSX.Element => {
     const [loaded, setLoaded] = useState<boolean>(false);
     const [totalPosts, setTotalPosts] = useState<number>(0);
     const [posts, setPosts] = useState<Array<Post>>([]);
-    const [categories, setCategories] = useState<Array<string>>([]);
 
     const changeCategoryFilter = (category: string) => {
         setLoaded(false);
@@ -35,9 +31,8 @@ const App = (): JSX.Element => {
 
     useEffect(() => {
         setLoaded(false);
-        const fetchPosts = async() => {
-            const { posts, total } = await getPosts();
-            console.log(posts, total);
+        const fetchPosts = async () => {
+            const { posts, total } = categoryFilter === 'All'? await getPosts() : await getPostsByCategory(categoryFilter); 
             const start = (page - 1) * pageSize;
             const end = start + pageSize;
             setTotalPosts(total);
@@ -45,20 +40,11 @@ const App = (): JSX.Element => {
             setTimeout(() => setLoaded(true), 1000);
         }
         fetchPosts();
-    }, [page]);
-
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const categories = await getCategories();
-            setCategories(categories);
-        }
-        fetchCategories();
-    }, []);
+    }, [categoryFilter, page]);
 
     return (
         <div className={s.app}>
             <Options
-                categories={categories}
                 filter={categoryFilter}
                 changeCategoryFilter={(category) => changeCategoryFilter(category)}
             />
@@ -73,7 +59,6 @@ const App = (): JSX.Element => {
                 pageSize={pageSize}
                 onChange={(pageNumber) => changePage(pageNumber)}
             />
-            <ScrollToTop smooth component={<FontAwesomeIcon icon={faCaretUp} />} />
         </div>
     );
 }
